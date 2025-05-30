bf6623c804227f1cbec79604e02cf202b3a0b577
sergio-wq <sergioruiz444@gmail.com>
2025-05-30 16:05:40 -0500

    cambios en auth.controller.js y usuario.model.js

--- models/usuario.model.js
+++ models/usuario.model.js
@@ -3,13 +3,15 @@ const db = require('../config/database');
 const Usuario = {
     crear: (usuario, callback) => {
         const query = `
-        INSERT INTO usuario (Nombre, Apellido, Correo, Contraseña, Id_Rol)
-        VALUES (?, ?, ?, ?, ?)
+        INSERT INTO usuario (Nombre, Apellido, Correo, Direccion, Telefono, Contraseña, Id_Rol)
+        VALUES (?, ?, ?, ?, ?, ?, ?)
         `;
         db.query(query, [
             usuario.Nombre,
             usuario.Apellido,
             usuario.Correo,
+            usuario.Direccion,
+            usuario.Telefono,
             usuario.Contraseña,
             usuario.Id_Rol
            ], callback); 
